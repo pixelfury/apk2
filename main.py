c80766a1b99b9b4241d2cc42b1521ee26b4f8e0b
pixelfury <52672501+pixelfury@users.noreply.github.com>
2019-07-27 20:03:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -59,17 +59,6 @@ class FoxxyApp(BoxLayout):
 class FoxxxyApp(App):
     def build(self):
         return FoxxyApp()
-       
-def reset():
-    import kivy.core.window as window
-    from kivy.base import EventLoop
-    if not EventLoop.event_listeners:
-        from kivy.cache import Cache
-        window.Window = window.core_select_lib('window', window.window_impl, True)
-        Cache.print_usage()
-        for cat in Cache._categories:
-            Cache._objects[cat] = {}
 
 if __name__ == '__main__':
-   reset()
    FoxxxyApp().run()
